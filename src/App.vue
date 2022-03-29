6b11c086ed24a5c724292f7c1c2098dd899b153c
Madeleine Di Mauro <damaris.dmauro@gmail.com>
2022-03-29 19:25:03 -0300

    Modificación en Home Vue, creación de archivo env y modificación de archivo gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
+<!-- Dentro del template solo puede haber un elemento -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <home-vue></home-vue>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HomeVue from "./components/HomeVue.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    HomeVue,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/HomeVue.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="HomeVue">
+    <b-container>
+      <b-row>
+        <b-col>
+          <h1>
+            Bienvenidos al sistema de facturación
+            <small>Hecho por Maddie</small>
+          </h1>
+          <img
+            src="../assets/facturacion-banner.jpg"
+            alt=""
+            width="100%"
+            height="auto"
+            rounded="circle"
+          />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col v-for="red in redes" :key="red.red" class="text-center">
+          <b-link :href="red.url" target="_blank">
+            <b-alert :variant="red.color" show>{{ red.red }}</b-alert>
+          </b-link>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeVue",
+  data() {
+    return {
+      redes: [
+        {
+          red: "facebook",
+          url: "https://facebook.com",
+          color: "info",
+        },
+        {
+          red: "twitter",
+          url: "https://twitter.com",
+          color: "warning",
+        },
+        {
+          red: "youtube",
+          url: "https://youtube.com",
+          color: "danger",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
